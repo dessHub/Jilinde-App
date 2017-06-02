58015479f61a972424f17cd9fc4c0ed8616f0203
dessHub <deskip95@gmail.com>
2017-06-02 16:02:38 +0300

    g

--- routes/ussd.js
+++ routes/ussd.js
@@ -24,13 +24,13 @@ module.exports = {
 
   function ussdloop(){
 
-  if (text === '') {
+  if (text === '' || text === '2*1*00' || text === '2*2*00' || text === '2*3*00') {
 	 message = "CON Welcome to Jilinde. Press\n";
      message += "1: To Report \n";
      message += "2: How To Get Justice.\n";
      message += "0: Exit";
   }
-  else if (text === '1') {
+  else if (text === '1' || text === '2*1*00*1' || text === '2*2*00*1' || text === '2*3*00*1') {
         message = 'CON Select Gategory.\n';
 				message += "1: Rape Case \n";
 				message += "2: Child Abuse \n";
@@ -50,10 +50,12 @@ module.exports = {
 				message += "3: Gender Based Violence \n";
 				message += "0: Exit \n";
   }
-	else if (text === '2*1') {
-		message = 'END Place a report on via one of our platforms (mobile app, website or ussd).On reporting include your mobile No.Jilinde will asign one of its lawyers to your case and we will contact with more details.\n';
+	else if (text === '2*1' || text === '2*2' || text === '2*3') {
+		message = 'CON Place a report on via one of our platforms (mobile app, website or ussd).On reporting include your mobile No.Jilinde will asign one of its lawyers to your case and we will contact with more details.\n';
+		message += '00. Back.\n';
+		message += '0. Exit.';
 	}
-	else if (text === '0' || text === '1*0' || text === '2*0') {
+	else if (text === '0' || text === '1*0' || text === '2*0' || text === '2*1*0' || text === '2*2*0' || text === '2*3*0') {
 		message = "END Thanks for trusting Jilinde."
 	}
 
